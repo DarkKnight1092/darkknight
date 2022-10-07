beb96c3214c3398c63fdac6009044f9f9da451f5
Knight <kota.mahendrareddy@gmail.com>
2022-10-07 23:21:30 +0530

    commitoct10

--- src/main/java/Lists.java
+++ src/main/java/Lists.java
@@ -9,28 +9,9 @@ import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class Lists {
+    public static void main(String[] args) throws CloneNotSupportedException {
 
-    public static void main(String[] args) {
 
-       Map l= new HashMap();
-//       l.add("Snake");
-//       l.add(1453);
-        System.out.println(l.get(0)+"         "+l.get(1));
 
     }
-
 }
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-class Carry {
-    String name;
-    String place;
-
-//    @Override
-//    public String toString(){
-//        return this.name+"  "+this.place;
-//}
-    }
